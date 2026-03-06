f1e9cee7b6b0180aee1bd0f672573b01a7678325
kaysee clintone <kayseeclintone@gmail.com>
2026-03-06 13:06:26 +0300

    Update script.js

--- final-project/script.js
+++ final-project/script.js
@@ -1,5 +1,18 @@
-const btn = document.getElementById("contactBtn");
+// scroll to projects when hero button clicked
 
-btn.addEventListener("click", () => {
-  alert("Thanks for visiting my portfolio!");
+const ctaBtn = document.getElementById("ctaBtn");
+
+ctaBtn.addEventListener("click", () => {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+
+// contact button alert
+
+const contactBtn = document.getElementById("contactBtn");
+
+contactBtn.addEventListener("click", () => {
+  alert("Thanks for visiting! You can reach me at hello@email.com");
 });
